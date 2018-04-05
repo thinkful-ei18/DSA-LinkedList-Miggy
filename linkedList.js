4718585514110389d26083a83729e5292bd028ba
emptyelephants <emptyelephants@gmail.com>
2018-04-05 12:57:38 -0700

    Reverse Reccursive done.

--- linkedList.js
+++ linkedList.js
@@ -6,7 +6,7 @@ class _Node{
   }
 }
 
-class linkedList{
+class LinkedList{
   constructor(){
     this.head=null;
   }
@@ -49,17 +49,17 @@ class linkedList{
 
   }
   find(data){
-    if(this.head === null){
+    if (this.head === null){
       console.log(data + ' that doesn\'t exist in the list.');
       return false;
     }
-    if(this.head.value === data){
+    if (this.head.value === data){
       console.log(data +' exists in the list.');
       return true;
     }
     let currentNode = this.head;
-    while(currentNode.next !== null){
-      if(currentNode.value === data){
+    while (currentNode.next !== null){
+      if (currentNode.value === data){
         console.log(data +' exists in the list.');
         return true;
       }
@@ -68,7 +68,7 @@ class linkedList{
 
   }
   insertBefore(data,key){
-    if(this.head === null){
+    if (this.head === null){
       return false;
     }
     if (this.head.value === key){
@@ -81,7 +81,7 @@ class linkedList{
       prevNode = currentNode;
       currentNode = currentNode.next;
     }
-    if(currentNode.value === key){
+    if (currentNode.value === key){
       prevNode.next = new _Node(data,currentNode);
       return true;
     }
@@ -89,73 +89,221 @@ class linkedList{
     return false;
   }
   insertAfter(data,key){
-    if(this.head === null){
+    if (this.head === null){
       return false;
     }
-    if(this.head.next === null){
+    if (this.head.next === null){
       this.insertLast(data);
       return;
     }
     let tempNode = new _Node(data,null);
     let currentNode = this.head;
-    while (currentNode.next!==null && currentNode.value !== key){
+    while (currentNode.next !== null && currentNode.value !== key){
       currentNode = currentNode.next;
     }
-    if(currentNode.value === key){
+    if (currentNode.value === key){
       tempNode.next = currentNode.next ;
       currentNode.next = tempNode;
       return true;
     }
     console.log('what you\'re trying to insert after doesn\'t exist');
     return false;
-
   }
-  insertAt(data,index){
-    let currentCount = 0;
-    if(this.head === )
-  }
-  display(){
-    console.log('===start of list===');
-    let tempNode = this.head;
-    if(tempNode === null){
-      console.log('its an empty list!');
+  insertAt(data, index = 0){
+    if (index === 1){
+      this.insertFirst(data);
       return;
     }
-    if(tempNode.next === null){
-      console.log(tempNode.value);
-      return;
+    if (this.head === null){
+      console.log('That index does not exist!');
+      return false;
     }
-    while(tempNode !== null){
-      console.log(tempNode.value);
-      tempNode = tempNode.next;
 
+
+    let currentNode = this.head;
+
+    let count = 1;
+    while(count < index && currentNode !== null){
+      currentNode = currentNode.next;
+      count ++;
     }
-    console.log('===end of list===');
+    if(currentNode === null){
+      console.log('Can\'t insert '  + data+ ' to index ' +index+ '. That index is outside the of the list!');
+      return false;
+    }
+    else{
+      this.insertBefore(data,currentNode.value);
+    }
+
   }
+
+
+}
+//helpers / suplemental
+
+function display(list){
+  console.log('===start of list===');
+  let displayPointer = list.head;
+  if(displayPointer === null){
+    console.log('its an empty list!');
+    return;
+  }
+  if(displayPointer.next === null){
+    console.log(displayPointer.value);
+    console.log('===end of list (there was only one)===');
+    return;
+  }
+  while(displayPointer !== null){
+    console.log(displayPointer.value);
+    displayPointer = displayPointer.next;
+
+  }
+  console.log('===end of list===');
+}
+
+function size(list){
+  let sizePointer = list.head;
+  let count = 0;
+  while(sizePointer !== null){
+    count++;
+    sizePointer = sizePointer.next;
+  }
+  console.log(`The Size of the list you me is ${count} nodes.`);
+  return count;
+}
+
+function isEmpty(list){
+  return list.head === null ? true : false;
+}
+
+function findPrevious(list, key){
+  if (isEmpty(list)){
+    console.log('Can\'t run this on an empty list');
+    return null;
+  }
+  let currentNode = list.head;
+  let prevNode = null;
+  while(currentNode.next !== null && currentNode.value!== key){
+    prevNode = currentNode;
+    currentNode = currentNode.next;
+  }
+  if (currentNode.value !== key){
+    console.log('The key provided doesnt exist in the list.');
+    return null;
+  }
+  if (prevNode === null){
+    console.log('There is nothing before the start of the list.');
+    return null;
+  }
+  console.log(`The previous of ${key} is ${prevNode.value}`);
+  return prevNode.value;
+}
+function findLast(list){
+  if (isEmpty(list)){
+    console.log('Thats an empty list!');
+    return null;
+  }
+  if (size(list) === 1){
+    return list.head;
+  }
+
+  let currentNode = list.head;
+  while(currentNode.next !== null){
+    currentNode = currentNode.next;
+  }
+  console.log('the last item in that list is '+ currentNode.value);
+  return currentNode;
+
 }
 
 function main(){
-  let SLL = new linkedList();
+  let SLL = new LinkedList();
   console.log('initial insert');
   SLL.insertLast('Apollo');
   SLL.insertLast('Boomer');
   SLL.insertLast('Helo');
   SLL.insertLast('Husker');
   SLL.insertLast('Starbuck');
-  SLL.display();
+  display(SLL);
   console.log('insert Tauhida at start of list');
   SLL.insertFirst('Tauhida');
   console.log('trying to remove a non existent');
   SLL.remove('squirrel');
-  SLL.display();
+  display(SLL);
   console.log('inserting Athena before Boomer');
   SLL.insertBefore('Athena','Boomer');
   console.log('inserting Hotdog after Helo');
   SLL.insertAfter('Hotdog','Helo');
-  SLL.display();
+  display(SLL);
+  console.log('Inserting Kat at the 3rd pos.');
+  SLL.insertAt('Kat',3);
+  display(SLL);
+  console.log('Removing Tauhida from the list');
+  SLL.remove('Tauhida');
+  display(SLL);
+  console.log('Counting the list.');
+  size(SLL);
+
+  //making an empty list and testing some functions on that empty list;
+  let EMPTYLIST = new LinkedList();
+  console.log(isEmpty(EMPTYLIST));
+  console.log(isEmpty(SLL));
+  size(EMPTYLIST);
+  console.log('Finding Previous of Kat in list');
+  findPrevious(SLL,'Kat');
+  console.log('Finding Previous of Head of list');
+  findPrevious(SLL,'Apollo');
+  console.log('Finding Previous of Last of list');
+  findPrevious(SLL,'Starbuck');
+
+  display(SLL);
+  findLast(SLL);
+}
+//WHAT THE MYSTERY FUCNION DOES IS IT REMOVES ALL DUPLICATES.
+//====================AFTERNOON WORK====================
+function reverseMain(){
+  let SLL = new LinkedList();
+  // console.log('initial insert');
+  SLL.insertLast('Apollo');
+  SLL.insertLast('Boomer');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
+  SLL.insertLast('Starbuck');
+  // console.log('before reverse');
+  // display(SLL);
+  display(SLL);
+  console.log('>>=========================entered rec list');
+  reverseList(SLL);
+  console.log('<<=========================exited rec list');
+  display(SLL);
+
+
+// null => nodeA => nodeB => null
+//null <= nodeA <= nodeB
+
+// nodeA => nodeB => nodeC
 
 }
 
 
+function reverseList(list, oldHead = null){
+  //base case
+  if(list.head.next === null){
+    console.log('base case!');
+    list.head.next = oldHead;
+    return list.head;
+  }
+  let prevHead = list.head;
+  list.head = list.head.next;
+  prevHead.next = oldHead;
+  return reverseList(list,prevHead);
 
-main();
+}
+  //  Apollo => Boomer=> Helo=> Husker=> Starbuck
+  //  Boomer => Helo=> Husker=> Starbuck
+  //  Helo => Husker => Starbuck
+  //  Husker => Starbuck
+  //  Starbuck
+
+// main();
+reverseMain();
